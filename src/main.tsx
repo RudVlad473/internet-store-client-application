23aa5a74d0635563a8e5e3411234fb425c0e6302
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-04-19 18:02:25 +0300

    added redux toolkit providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { ApiProvider } from "@reduxjs/toolkit/dist/query/react"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { Provider } from "react-redux"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import App from "./App"
+import "./index.scss"
+import { apiSlice } from "./shared/api"
+import { store } from "./shared/lib/store"
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ApiProvider api={apiSlice}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ApiProvider>
+  </React.StrictMode>
 )
